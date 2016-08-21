92439d80914f120b82f34b5243d3bf880a8b4cc8
Marija Jankovic <marijajankovic623@gmail.com>
2016-08-21 17:35:11 +0200

    Veb sajt pre igranja sa toogle navbarom za proizvode

---
+++ web/js/myscript.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+        $("#action").mouseenter(
+            function () {
+                $("#reaction").slideDown('medium');
+            }
+        );
+
+
+    }
+);
+
+$(document).ready(function () {
+
+        $("#reaction").mouseleave(
+            function () {
+                $("#reaction").slideUp('medium');
+            }
+        );
+
+
+    }
+);
+
+$(document).ready(function () {
+
+        $("#navigation").mouseleave(
+            function () {
+                $("#reaction").slideUp('medium');
+            }
+        );
+
+
+    }
+);
