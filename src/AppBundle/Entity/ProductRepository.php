9bf5bac64e50daded86f493e8ce0ad33bfe71251
Marko Kastratovic <marko.r.kastratovic@gmail.com>
2016-09-30 12:52:20 +0200

    Pagination without view

--- src/AppBundle/Entity/ProductRepository.php
+++ src/AppBundle/Entity/ProductRepository.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * ProductRepository
@@ -10,9 +11,9 @@ use Doctrine\ORM\EntityRepository;
  */
 class ProductRepository extends EntityRepository
 {
-    public function findProductsByAnimal($animal)
+    public function findProductsByAnimal($animal, $page)
     {
-        return $this->createQueryBuilder('p')
+        $query = $this->createQueryBuilder('p')
             ->join('p.subcategory', 's')
             ->join('s.category', 'c')
             ->join('c.animal', 'a')
@@ -20,13 +21,14 @@ class ProductRepository extends EntityRepository
             ->setParameters(array(
                 'animal' => $animal,
             ))
-            ->getQuery()
-            ->getResult();
+            ->getQuery();
+
+        return $this->paginate($query, $page);
     }
 
-    public function findProductsByTitleEng($animal, $category, $subcategory)
+    public function findProductsByTitleEng($animal, $category, $subcategory, $page)
     {
-        return $this->createQueryBuilder('p')
+        $query = $this->createQueryBuilder('p')
             ->join('p.subcategory', 's')
             ->join('s.category', 'c')
             ->join('c.animal', 'a')
@@ -38,13 +40,14 @@ class ProductRepository extends EntityRepository
                 'category'    => $category,
                 'animal'      => $animal,
             ))
-            ->getQuery()
-            ->getResult();
+            ->getQuery();
+
+        return $this->paginate($query, $page);
     }
 
-    public function findProductsByTitleEngAll($animal, $category)
+    public function findProductsByTitleEngAll($animal, $category, $page)
     {
-        return $this->createQueryBuilder('p')
+        $query = $this->createQueryBuilder('p')
             ->join('p.subcategory', 's')
             ->join('s.category', 'c')
             ->join('c.animal', 'a')
@@ -54,7 +57,36 @@ class ProductRepository extends EntityRepository
                 'category' => $category,
                 'animal'   => $animal,
             ))
-            ->getQuery()
-            ->getResult();
+            ->getQuery();
+
+        return $this->paginate($query, $page);
+    }
+
+    /**
+     * Paginator Helper
+     *
+     * Pass through a query object, current page & limit
+     * the offset is calculated from the page and limit
+     * returns an `Paginator` instance, which you can call the following on:
+     *
+     *     $paginator->getIterator()->count() # Total fetched (ie: `5` posts)
+     *     $paginator->count() # Count of ALL posts (ie: `20` posts)
+     *     $paginator->getIterator() # ArrayIterator
+     *
+     * @param Doctrine\ORM\Query $dql DQL Query Object
+     * @param integer $page Current page (defaults to 1)
+     * @param integer $limit The total number per page (defaults to 5)
+     *
+     * @return \Doctrine\ORM\Tools\Pagination\Paginator
+     */
+    public function paginate($dql, $page = 1, $limit = 9)
+    {
+        $paginator = new Paginator($dql);
+
+        $paginator->getQuery()
+            ->setFirstResult($limit * ($page - 1))// Offset
+            ->setMaxResults($limit); // Limit
+
+        return $paginator;
     }
 }
